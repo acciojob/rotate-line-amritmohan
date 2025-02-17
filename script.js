6984cd00cd20c3db192351cc3cdc5eae7287bef0
@amritmohan <amritmohan2284@gmail.com>
2025-02-17 10:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+
+let angle = 0;
+function rotateLine() {
+  // Increase the angle by 2 degrees
+  angle += 2;
+  
+  // Apply the rotation using the CSS transform property
+  line.style.transform = `rotate(${angle}deg)`;
+}
+
+// Set an interval to update the rotation every 20 milliseconds
+setInterval(rotateLine, 20);
